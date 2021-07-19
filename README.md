c636407b77ace8db7c3aa2b01c2bbf916b36b8ff
Chidera Ike <chideraike.s@gmail.com>
2021-07-19 11:08:51 +0000

    :memo: added information about the project

---
+++ README.md
@@ -0,0 +1,22 @@
+# Payment App
+
+Design inspiration gotten from [here](https://dribbble.com/shots/14210557-Finance-Mobile-Application-UX-UI-Design).
+
+## Installation
+This app was built using [React Native](https://reactnative.dev/) with [Expo](https://expo.io). In other to run it install **Expo Go** from either the App Store or Play Store.  
+
+ -  Run the app [here]() directly from the Expo app
+
+Then follow these steps to run the app locally;
+
+ 1. Download or clone this repo
+2. Run  `npm install`  or  `yarn`  in the project folder directory on the terminal
+3. After all node modules are done downloading you can run the app by using  `npm start`  or  `yarn start`  on the terminal
+4. Follow the instructions from there to connect your device (iOS/Andriod)
+
+## Features
+
+Features currently on the app;
+
+- [x]  Screens
+- [x]  [Navigation](https://reactnavigation.org/docs/getting-started)
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, View, SafeAreaView } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 
 import NewText from '../components/NewText'
 import NewButton from '../components/NewButton'
@@ -7,15 +8,53 @@ import NewButton from '../components/NewButton'
 export default function Home({ navigation }) {
     return (
         <SafeAreaView style={styles.container}>
-            <NewText>Home Screen</NewText>
+            <UserInfo />
             <NewButton title="Transactions" onClick={() => navigation.navigate('Transactions')} />
         </SafeAreaView>
     )
 }
+
+const UserInfo = () => (
+    <View style={styles.userInfoContainer}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Ionicons name="reorder-two-outline" size={24} color="#0d0f35" />
+            <Ionicons name="ellipsis-vertical-outline" size={24} color="#0d0f35" />
+        </View>
+        <View style={styles.userInfoPicture}></View>
+        <View style={{ alignItems: 'center' }}>
+            <NewText h2 bold primary>Hira Riaz</NewText>
+            <NewText h5>UX/UI Designer</NewText>
+        </View>
+        <View></View>
+    </View>
+)
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#e9f0fb',
     },
+    userInfoContainer: {
+        backgroundColor: '#fff',
+        margin: 20,
+        padding: 20,
+        borderRadius: 30,
+        shadowColor: "#b0c6e1",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
+    },
+    userInfoPicture: {
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        backgroundColor: '#aa7465',
+        alignSelf: 'center',
+        marginTop: 20,
+        marginBottom: 20,
+    }
 })

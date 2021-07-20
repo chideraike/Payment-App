cebc110bf3475a20714f41c82256f84f6115e800
Chidera Ike <chideraike.s@gmail.com>
2021-07-20 17:54:38 +0000

    :construction: on the home screen

--- screens/Home.js
+++ screens/Home.js
@@ -9,6 +9,12 @@ export default function Home({ navigation }) {
     return (
         <SafeAreaView style={styles.container}>
             <UserInfo />
+            <View>
+                <View>
+                    <NewText primary bold h2>Overview</NewText>
+                </View>
+                <View></View>
+            </View>
             <NewButton title="Transactions" onClick={() => navigation.navigate('Transactions')} />
         </SafeAreaView>
     )
@@ -25,7 +31,20 @@ const UserInfo = () => (
             <NewText h2 bold primary>Hira Riaz</NewText>
             <NewText h5>UX/UI Designer</NewText>
         </View>
-        <View></View>
+        <View style={styles.statsContainer}>
+            <Stat name="Income">8900</Stat>
+            <View style={styles.line} />
+            <Stat name="Expenses">5500</Stat>
+            <View style={styles.line} />
+            <Stat name="Loan">890</Stat>
+        </View>
+    </View>
+)
+
+const Stat = ({ name, children }) => (
+    <View style={{ alignItems: 'center' }}>
+        <NewText primary h3>${children}</NewText>
+        <NewText p>{name}</NewText>
     </View>
 )
 
@@ -56,5 +75,16 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         marginTop: 20,
         marginBottom: 20,
-    }
+    },
+    statsContainer: {
+        padding: 30,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    line: {
+        width: 1,
+        height: 50,
+        backgroundColor: 'grey',
+        opacity: 0.5
+    },
 })
